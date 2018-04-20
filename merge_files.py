925735561a86a87adce2a855e1ef2350ca8e8090
Hoang Long <litpuvn@gmail.com>
2018-04-20 17:21:19 -0500

    merge files

---
+++ merge_files.py
@@ -0,0 +1,22 @@
+from os import listdir
+from os.path import isfile, join
+
+
+mypath = 'output'
+filenames = [join(mypath, f) for f in listdir(mypath) if isfile(join(mypath, f))]
+
+header = ["anger","anticipation","disgust","fear","joy","sadness","surprise","trust"]
+with open('merge_comments.csv', 'w') as outfile:
+    outfile.write(','.join(header) + '\n')
+
+    for fname in filenames:
+        if '.out.' not in fname:
+            continue
+
+        with open(fname) as infile:
+            in_header = True
+            for line in infile:
+                if in_header:
+                    in_header = False
+                    continue
+                outfile.write(line)
